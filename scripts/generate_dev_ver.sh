496735969dde8daf8c35d48061b862580c3edf19
Nev Ball <neville.ball@dubber.net>
2026-02-25 11:45:17 +1100

    fix scripts

--- scripts/generate_dev_ver.sh
+++ scripts/generate_dev_ver.sh
@@ -23,10 +23,10 @@ NEW_VERSION="${VERSION}.dev+${GIT_HASH}"
 # Update version in pyproject.toml and lisette/__init__.py
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
-    sed -i '' "s/^version = \".*\"/version = \"$NEW_VERSION\"/" "$DIRECTORY/pyproject.toml"
-    sed -i '' "s/^__version__ = \".*\"/__version__ = \"$NEW_VERSION\"/" "$DIRECTORY/lisette/__init__.py"
+    sed -i '' "s|^version = \".*\"|version = \"$NEW_VERSION\"|" "$DIRECTORY/pyproject.toml"
+    sed -i '' "s|^__version__ = \".*\"|__version__ = \"$NEW_VERSION\"|" "$DIRECTORY/lisette/__init__.py"
 else
     # Linux
-    sed -i "s/^version = \".*\"/version = \"$NEW_VERSION\"/" "$DIRECTORY/pyproject.toml"
-    sed -i "s/^__version__ = \".*\"/__version__ = \"$NEW_VERSION\"/" "$DIRECTORY/lisette/__init__.py"
+    sed -i "s|^version = \".*\"|version = \"$NEW_VERSION\"|" "$DIRECTORY/pyproject.toml"
+    sed -i "s|^__version__ = \".*\"|__version__ = \"$NEW_VERSION\"|" "$DIRECTORY/lisette/__init__.py"
 fi
